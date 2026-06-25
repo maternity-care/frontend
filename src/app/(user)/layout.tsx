0745eacbeaa3835c841ac7048181b6191283f5e1
NguyenSwan <nhiphongtich@gmail.com>
2026-06-25 21:54:02 +0700

    update: AuthGuard layout

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,12 +1,14 @@
 import type { ReactNode } from "react";
 import { AntdThemeProvider } from "@/providers/AntdThemeProvider";
+import { UserShell } from "@/fe/components/layout/user/UserShell";
+import { AuthGuard } from "@/fe/components/layout/AuthGuard";
 
 export default function UserLayout({ children }: { children: ReactNode }) {
   return (
     <AntdThemeProvider mode="user">
-      <div className="flex min-h-screen items-center justify-center bg-pink-50">
-        {children}
-      </div>
+      <AuthGuard>
+        <UserShell>{children}</UserShell>
+      </AuthGuard>
     </AntdThemeProvider>
   );
 }
